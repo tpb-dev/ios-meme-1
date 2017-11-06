c73056686ded3c064410ff295abbcbdc002b1e4f
Randall Tom <randalltom@sfo315.ca.revelsystems.com>
2017-11-06 11:04:56 -0800

    1

---
+++ meme1/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  ios-meme-me-editor-1.0
+//
+//  Created by Randall Tom on 11/3/17.
+//  Copyright © 2017 Randall Tom. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topTextField: String!
+    var bottomTextFiield: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
+
